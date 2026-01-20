c37db560d94bab01904ed7021fa773732694b033
benjaminlarteynssecrime-del <benjamin.lartey.nss.ecrime@gmail.com>
2026-01-20 16:50:18 +0000

    Changed back to Supabase

--- Food Menu/src/lib/db.ts
+++ Food Menu/src/lib/db.ts
@@ -2,11 +2,8 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: Number(process.env.PGPORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Required for Supabase
 });
 
 export default pool;
