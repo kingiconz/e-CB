70d8392bfe56d0c1aaff556db784d118a98066fd
benjaminlarteynssecrime-del <benjamin.lartey.nss.ecrime@gmail.com>
2026-01-20 10:50:42 +0000

    Few Changes

--- Food Menu/next.config.mjs
+++ Food Menu/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true, // ✅ enables App Router
+  },
+  output: 'standalone',   // optional, good for production
+};
 
 export default nextConfig;
