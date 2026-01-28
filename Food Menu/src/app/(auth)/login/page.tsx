652d6bbae376f95b74abf37fbfc84acc3d5f43ab
benjaminlarteynssecrime-del <benjamin.lartey.nss.ecrime@gmail.com>
2026-01-28 12:26:12 +0000

    Updated landing images and staff login name

--- Food Menu/src/app/(auth)/login/page.tsx
+++ Food Menu/src/app/(auth)/login/page.tsx
@@ -85,7 +85,7 @@ export default function LoginPage() {
               htmlFor="username"
               className="block text-xs sm:text-sm font-medium text-gray-700 mb-1 sm:mb-2"
             >
-              Username
+              Full Name
             </label>
             <input
               id="username"
@@ -96,7 +96,7 @@ export default function LoginPage() {
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               className="w-full px-3 sm:px-4 py-2 border border-gray-300 rounded-md sm:rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm"
-              placeholder="Enter your username"
+              placeholder="Enter your Fullname"
             />
           </div>
 
